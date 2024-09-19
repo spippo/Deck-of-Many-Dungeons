94096ab7e5a2b6dba89f22906937e0338ff1d335
spippo <48952044+spippo@users.noreply.github.com>
2024-09-19 20:35:13 +0200

    Delete rotateCards.js

--- rotateCards.js
+++
@@ -1,8 +0,0 @@
-function a() {
-  carta.style = "transform: rotate(-90deg)";
-}
-
-const carta = document.getElementsByClassName("card");
-for (let i = 0; i < carta.length; i++) {
-  carta[i].addEventListener("click", a);
-}
